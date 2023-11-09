58020ff4ae4621e63720ae4bd326d0f27b65bc9a
Hongseunghyun <teddy@another-space.co.kr>
2023-11-09 15:52:28 +0900

    20231109

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Total Sample Test Page 2023
+// HSD Project index.js 
+import React from "react";
+import NavigationSide from "./components/navigation_side";
+import ReactDOM from "react-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <NavigationSide />
+  </React.StrictMode>
+);
