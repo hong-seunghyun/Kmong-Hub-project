803718f9cbc242a23fba05a6255d60c0358bde4c
Hongseunghyun <teddy@another-space.co.kr>
2023-11-21 15:53:41 +0900

    20231121

--- next.config.js
+++ next.config.js
@@ -1,11 +1,1 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-const nextEnv = require("next-env");
-
-const withEnv = nextEnv();
-
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    console.log("dev mode")
-  }
-  return withEnv(defaultConfig)
-}
+//
