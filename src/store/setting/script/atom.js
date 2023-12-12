0532e904575a30edef2d545272f7ca1238efe2dc
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-12 20:16:27 +0900

    Add : terms, scritps atoms

---
+++ src/store/setting/script/atom.js
@@ -0,0 +1,31 @@
+import { atom } from "recoil";
+
+export const SettingChntkScptCntn = atom({
+  key: "SettingChntkScptCntn",
+  default: "",
+});
+
+export const SettingGganltcScptCntn = atom({
+  key: "SettingGganltcScptCntn",
+  default: "",
+});
+
+export const SettingOgImgPathAddr = atom({
+  key: "SettingOgImgPathAddr",
+  default: "",
+});
+
+export const SettingSiteItrCntn = atom({
+  key: "SettingSiteItrCntn",
+  default: "",
+});
+
+export const SettingSiteNm = atom({
+  key: "SettingSiteNm",
+  default: "",
+});
+
+export const SettingSiteScptId = atom({
+  key: "SettingSiteScptId",
+  default: "",
+});
