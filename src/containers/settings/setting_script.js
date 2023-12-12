2ff6a8dfb763ca9a80c15b5d519aad40c577f15e
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-12 20:17:02 +0900

    fix : add atoms

--- src/containers/settings/setting_script.js
+++ src/containers/settings/setting_script.js
@@ -2,78 +2,135 @@ import React, { useState } from "react";
 import Tab from "/src/components/tabs/settings_tab_fixed";
 import Input from "/src/components/textFields/textInput.tsx";
 import TextArea from "/src/components/textFields/textArea.tsx";
-import Upload from "/src/components/upload/upload"
-import OutlineBtn from "/src/components/buttons/button_outline_l"
-import PrimaryBtn from "/src/components/buttons/button_primary_l"
+import Upload from "/src/components/upload/upload";
+import OutlineBtn from "/src/components/buttons/button_outline_l";
+import PrimaryBtn from "/src/components/buttons/button_primary_l";
+import { useRecoilState } from "recoil";
+import {
+  SettingChntkScptCntn,
+  SettingGganltcScptCntn,
+  SettingOgImgPathAddr,
+} from "../../store/setting/script/atom";
+import {
+  SettingSiteItrCntn,
+  SettingSiteNm,
+} from "../../store/setting/basic/atom";
 
 const Component = () => {
-	const [ subTab, setSubTab ] = useState(0);
-	const [ activeSubTab, setActiveSubTab ] = useState(0);
+  const [subTab, setSubTab] = useState(0);
+  const [activeSubTab, setActiveSubTab] = useState(0);
 
-	const TabContentA = () => {
-		setActiveSubTab(subTab);
-		return(
-			<div className="sub-page-0">
-				<Input labelText="사이트 이름" placeholder="사이트 이름을 입력해주세요." valueType="" helperTextResult="none" iconState="false"/>
-				<TextArea labelText="사이트 소개" placeholder="사이트 소개를 입력해 주세요." valueType="" helperTextResult="none"/>
-				<div className="flex_ button-input">
-				<Input labelText="키워드" placeholder="사이트의 내용과 관련된 주요 키워드를 입력해 주세요." valueType="" helperTextResult="none" iconState="false"/>
-					<OutlineBtn text="등록" />
-				</div>
-				<div className="favicon-wrap">
-					<p className="caption-B">OG 이미지</p>
-					<Upload state="default" type="normal"/>
-					<p className="caption-R helper-txt">
-						허용 사이즈: <span>1200px x 630px</span> | 파일 형식: <span>JPG, PNG</span>
-					</p>
-				</div>
-				
-			</div>
-		)
-	}
+  const TabContentA = () => {
+    setActiveSubTab(subTab);
+    const [ogImgPathAddr, setOgImgPathAddr] =
+      useRecoilState(SettingOgImgPathAddr);
+    const [siteItrCntn, setSiteItrCntn] = useRecoilState(SettingSiteItrCntn);
+    const [siteNm, setSiteNm] = useRecoilState(SettingSiteNm);
 
-	const TabContentB = () => {
-		setActiveSubTab(subTab);
-		return(
-			<div className="sub-page-1">
-				<TextArea labelText="스크립트" placeholder="스크립트를 삽입해 주세요." valueType="" helperTextResult="none"/>
-			</div>
-		)
-	}
+    return (
+      <div className="sub-page-0">
+        <Input
+          labelText="사이트 이름"
+          placeholder="사이트 이름을 입력해주세요."
+          valueType=""
+          state={siteNm}
+          setState={setSiteNm}
+          helperTextResult="none"
+          iconState="false"
+        />
+        <TextArea
+          labelText="사이트 소개"
+          placeholder="사이트 소개를 입력해 주세요."
+          valueType=""
+          state={siteItrCntn}
+          setState={setSiteItrCntn}
+          helperTextResult="none"
+        />
+        <div className="flex_ button-input">
+          <Input
+            labelText="키워드"
+            placeholder="사이트의 내용과 관련된 주요 키워드를 입력해 주세요."
+            valueType=""
+            state={siteNm}
+            setState={setSiteNm}
+            helperTextResult="none"
+            iconState="false"
+          />
+          <OutlineBtn text="등록" />
+        </div>
+        <div className="favicon-wrap">
+          <p className="caption-B">OG 이미지</p>
+          <Upload state="default" type="normal" />
+          <p className="caption-R helper-txt">
+            허용 사이즈: <span>1200px x 630px</span> | 파일 형식:{" "}
+            <span>JPG, PNG</span>
+          </p>
+        </div>
+      </div>
+    );
+  };
 
-	const TabContentC = () => {
-		setActiveSubTab(subTab);
-		return(
-			<div className="sub-page-2">
-				<TextArea labelText="스크립트" placeholder="스크립트를 삽입해 주세요." valueType="" helperTextResult="none"/>
-			</div>
-		)
-	}
+  const TabContentB = () => {
+    setActiveSubTab(subTab);
+    const [gganltcScptCntn, setGganltcScptCntn] = useRecoilState(
+      SettingGganltcScptCntn
+    );
+    return (
+      <div className="sub-page-1">
+        <TextArea
+          labelText="스크립트"
+          placeholder="스크립트를 삽입해 주세요."
+          valueType=""
+          state={gganltcScptCntn}
+          setState={setGganltcScptCntn}
+          helperTextResult="none"
+        />
+      </div>
+    );
+  };
 
-	const TabContents = () => {
-		if(subTab === 0){
-			return <TabContentA />
-		}else if(subTab === 1){
-			return <TabContentB />
-		}else if(subTab === 2){
-			return <TabContentC />
-		}
-	}
+  const TabContentC = () => {
+    setActiveSubTab(subTab);
+    const [chntkScptCntn, setChntkScptCntn] =
+      useRecoilState(SettingChntkScptCntn);
+    return (
+      <div className="sub-page-2">
+        <TextArea
+          labelText="스크립트"
+          placeholder="스크립트를 삽입해 주세요."
+          valueType=""
+          state={chntkScptCntn}
+          setState={setChntkScptCntn}
+          helperTextResult="none"
+        />
+      </div>
+    );
+  };
 
-	return(
-		<div className="container">
-			<div className="page-wrap">
-				<div className="setting-contents setting-script">
-					<h1 className="display-5-B">사이트 설정</h1>
-					<Tab setSubTab={setSubTab} activeSubTab={activeSubTab} active={3}/>
-					<TabContents />
-					<div className="button-wrap flex_">
-						<OutlineBtn text="초기화" state="default"/>
-						<PrimaryBtn text="저장" state="disabled"/>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
+  const TabContents = () => {
+    if (subTab === 0) {
+      return <TabContentA />;
+    } else if (subTab === 1) {
+      return <TabContentB />;
+    } else if (subTab === 2) {
+      return <TabContentC />;
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="page-wrap">
+        <div className="setting-contents setting-script">
+          <h1 className="display-5-B">사이트 설정</h1>
+          <Tab setSubTab={setSubTab} activeSubTab={activeSubTab} active={3} />
+          <TabContents />
+          <div className="button-wrap flex_">
+            <OutlineBtn text="초기화" state="default" />
+            <PrimaryBtn text="저장" state="disabled" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Component;
