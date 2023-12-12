3bb3c4324448e05d19319f73177b18f5d6c3a050
이재잰 <HINU0622@gmail.com>
2023-12-12 19:51:11 +0900

    쓸데없는 코드 제거

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,6 @@
 import Component from "/src/containers/home/index";
 import NavigationTopLogin from "/src/components/navigations/navigation_top_login";
 import NavigationSide from "/src/components/navigations/navigation_side";
-import { getUserInfo } from "../../asset/apis/siteApis";
-import { useEffect } from "react";
 
 const Page = () => (
   <>
@@ -11,34 +9,5 @@ const Page = () => (
     <Component />
   </>
 );
-export async function getServerSideProps() {
-  let auth;
-  useEffect(() => {
-    const token = localStorage.getItem('accessToken');
-    auth = `Bearer ${token}`;
-  })
-  let userData;
-  try {
-    await getUserInfo(auth).then(res => {
-      console.log(res);
-      userData = res;
-    }).catch(err => {
-      console.log(err);
-    });
-    return {
-      props: {
-        userData,
-      },
-    };
-  } catch(error) {
-    console.log(error);
-    return {
-      props: {
-        userData: null,
-        error: "Error fetching data.",
-      }
-    }
-  }
-}
 
 export default Page;
