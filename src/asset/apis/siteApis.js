0b20de774420f648555c963e0fd5ec5fe9dba045
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-12 10:20:48 +0900

    fix : axios -> custom axios

--- src/asset/apis/siteApis.js
+++ src/asset/apis/siteApis.js
@@ -1,33 +1,33 @@
-import axios from "axios";
+import { Api } from "../config/Api";
 import { baseUrl } from "../config/config.json";
 
 //get(사이트 기본 정보)
 export const getSiteBasicInfo = () => {
-  const response = axios.get(`${baseUrl}/viewapi/mngr/site/v1/basicInfo`);
+  const response = Api.get(`${baseUrl}/viewapi/mngr/site/v1/basicInfo`);
   return response;
 };
 
 //get(사이트 정보 확장)
 export const getSiteBsl = () => {
-  const response = axios.get(`${baseUrl}/viewapi/mngr/site/v1/bsl`);
+  const response = Api.get(`${baseUrl}/viewapi/mngr/site/v1/bsl`);
   return response;
 };
 
 //get(회원가입 기본 설정)
 export const getSiteSttg = () => {
-  const response = axios.get(`${baseUrl}/viewapi/mngr/site/v1/bcmem/sttg`);
+  const response = Api.get(`${baseUrl}/viewapi/mngr/site/v1/bcmem/sttg`);
   return response;
 };
 
 //get(사이트 약관 정보)
 export const getSiteClus = () => {
-  const response = axios.get(`${baseUrl}/viewapi/mngr/site/v1/clus`);
+  const response = Api.get(`${baseUrl}/viewapi/mngr/site/v1/clus`);
   return response;
 };
 
 //get(사이트 스크립트 조회)
 export const getSiteScpt = () => {
-  const response = axios.get(`${baseUrl}/viewapi/mngr/site/v1/scpt`);
+  const response = Api.get(`${baseUrl}/viewapi/mngr/site/v1/scpt`);
   return response;
 };
 
