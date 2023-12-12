44d0d43f5c2716d8c00ae7a8a119c3734b8d7b5f
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-12 16:11:06 +0900

    fix : Api

--- src/asset/config/Api.js
+++ src/asset/config/Api.js
@@ -7,8 +7,10 @@ export const Api = axios.create({
     "Cache-Control": "no-cache",
     Pragma: "no-cache",
     Expires: "0",
-    Authorization: `bearer ${
+    Authorization: `Bearer ${
       typeof window !== "undefined" && localStorage.getItem("accessToken")
     }`,
   },
 });
+
+//eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0MUBuYXZlci5jb20iLCJhdXRoIjoiUk9MRV9BRE1JTixST0xFX1VTRVIiLCJleHAiOjE3MDIzOTgxOTJ9.w7Sp7V4rYJGqgE_ac0P4plaa3woHa8AfgavHYqA0ZaRvTalNXPoYYUVhr0JsE_m9zKPARVbtgq0mA0PJTZU6NQ
