09b9eb0d5d68d66d01b3052788d55302ad2932ff
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-13 23:18:19 +0900

    feat : connect server (support)

--- src/containers/contents/support.js
+++ src/containers/contents/support.js
@@ -1,47 +1,88 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import Tab from "/src/components/tabs/content_tab_fixed";
 import Input from "/src/components/textFields/textInput.tsx";
 import OutlineBtn from "/src/components/buttons/button_outline_l";
 import PrimaryBtn from "/src/components/buttons/button_primary_l";
 import Icon from "/src/components/icon/icon.tsx";
+import { SupportListContent } from "../../store/contents/support/atom";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { getSupport } from "../../asset/apis/contentApis";
 
 const Component = () => {
   const [subTab, setSubTab] = useState(0);
   const [activeSubTab, setActiveSubTab] = useState(0);
 
+  const setSupportListContent = useSetRecoilState(SupportListContent);
+
+  const getSupportListContentFunc = async () => {
+    await getSupport()
+      .then((e) => setSupportListContent([...e.data.data]))
+      .catch((e) => console.log(e));
+  };
+
+  useLayoutEffect(() => {
+    getSupportListContentFunc();
+  }, []);
+
   const TabContents = () => {
+    const [supportListState, setSupportListState] = useState([]);
+    const [supportList, setSupportList] = useRecoilState(SupportListContent);
     setActiveSubTab(subTab);
+
+    useLayoutEffect(() => {
+      setSupportListState([...supportList]);
+    }, [supportList]);
+
+    const changeCategoryValue = ({ value, index }) => {
+      const copyList = JSON.parse(JSON.stringify(supportList));
+      //   console.log(copyList, index);
+      copyList[index].catgNm = value;
+      setSupportListState(copyList);
+      setSupportList(copyList);
+    };
     return (
       <div className="sub-page-0">
-        <div className="flex_ button-input">
-          <Input
-            importState=""
-            labelText="카테고리 1"
-            placeholder="카테고리 제목을 입력해 주세요."
-            valueType=""
-            helperTextResult="helperA"
-            iconState="false"
-          />
-          <OutlineBtn
-            text={<Icon icon="plus" size={16} color="#464749" stroke="none" />}
-          />
-        </div>
-        <div className="flex_ button-input">
-          <Input
-            importState=""
-            labelText="카테고리 2"
-            placeholder="카테고리 제목을 입력해 주세요."
-            valueType=""
-            helperTextResult="helperA"
-            iconState="false"
-          />
-          <OutlineBtn
-            text={<Icon icon="minus" size={3} color="#464749" stroke="none" />}
-          />
-          <OutlineBtn
-            text={<Icon icon="plus" size={16} color="#464749" stroke="none" />}
-          />
-        </div>
+        {supportListState.map((e, idx) => {
+          return (
+            <>
+              <div className="flex_ button-input">
+                <Input
+                  importState=""
+                  labelText={`카테고리 ${idx + 1}`}
+                  placeholder="카테고리 제목을 입력해 주세요."
+                  valueType=""
+                  state={e.catgNm}
+                  setStateFunc={(event) => {
+                    console.log(idx);
+                    changeCategoryValue({
+                      event: event.target.value,
+                      index: idx,
+                    });
+                  }}
+                  helperTextResult="helperA"
+                  iconState="false"
+                />
+                {idx !== 0 && (
+                  <OutlineBtn
+                    text={
+                      <Icon
+                        icon="minus"
+                        size={3}
+                        color="#464749"
+                        stroke="none"
+                      />
+                    }
+                  />
+                )}
+                <OutlineBtn
+                  text={
+                    <Icon icon="plus" size={16} color="#464749" stroke="none" />
+                  }
+                />
+              </div>
+            </>
+          );
+        })}
       </div>
     );
   };
