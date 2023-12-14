4fe2567803a1b874fed68115a58a8c3c96483358
이재잰 <HINU0622@gmail.com>
2023-12-14 22:21:29 +0900

    Api 완성

--- src/asset/apis/signup.js
+++ src/asset/apis/signup.js
@@ -7,8 +7,33 @@ export const checkTheEmail = ({ email }) => {
   return response;
 }
 
-// post(소속 검색)
+// get(소속 검색)
 export const searchOrgn = ({ query }) => {
   const response = ApiOrgn.get(`/organ?page=1&row_count=3&is_using=true&query=${query}&order_by=id&order_sort=id`);
+  return response;
+}
+
+// post(회원가입)
+export const register = ({ emailAddr, hpNo, mbrNm, nnmNm, nwlAgrmYn, pwd, smsRcvAgrmYn, ucmdCd, orgnPhcNo }, pflImg, pslAttchFile) => {
+  const registerDTO = {
+    emailAddr,
+    hpNo,
+    mbrNm,
+    nnmNm,
+    nwlAgrmYn,
+    pwd,
+    smsRcvAgrmYn,
+    ucmdCd,
+    orgnPhcNo
+  };
+  const formData = new FormData();
+  formData.append('pflImg', pflImg);
+  formData.append('pslAttchFile', pslAttchFile);
+  formData.append('signUpReq', registerDTO);
+  const response = Api.post(`/viewapi/mngr/site/v2/member`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    }
+  });
   return response;
 }
