3749ebb4473e157245100f83480d30e532b376ad
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-14 20:06:18 +0900

    test : add tiny test

--- src/components/editorBox/index.js
+++ src/components/editorBox/index.js
@@ -1,8 +1,10 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 
 export default function App() {
   const editorRef = useRef(null);
+  const initialText = "<p>This is the initial content of the editor.</p>";
+  const [text, setText] = useState(initialText);
   // const log = () => {
   // 	if (editorRef.current) {
   // 		console.log(editorRef.current.getContent());
@@ -12,7 +14,7 @@ export default function App() {
     <>
       <Editor
         onInit={(evt, editor) => (editorRef.current = editor)}
-        initialValue="<p>This is the initial content of the editor.</p>"
+        initialValue={initialText}
         init={{
           height: 500,
           menubar: false,
@@ -29,7 +31,9 @@ export default function App() {
           content_style:
             "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
         }}
+        onEditorChange={(newText) => setText(newText)}
       />
+      {text}
     </>
   );
 }
