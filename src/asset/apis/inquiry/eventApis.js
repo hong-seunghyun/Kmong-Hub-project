3ea37949a1bb971e3d7a13dd0b72630628d2ee0c
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-15 01:57:28 +0900

    feat : apis

---
+++ src/asset/apis/inquiry/eventApis.js
@@ -0,0 +1,25 @@
+import { Api } from "../config/Api";
+
+export const getEventList = ({ currentIdx, searchType, search }) => {
+  const response = Api.get(
+    `/viewapi/mngr/site/v1/inquiry/even?currentPage=${currentIdx}&limit=10${
+      searchType && `&searchType=${searchType}`
+    }&searchValue=${search}`
+  );
+  return response;
+};
+
+export const getEventDetail = ({ iqryNo }) => {
+  const response = Api.get(
+    `/viewapi/mngr/site/v1/inquiry/evenDetail?iqryNo=${iqryNo}`
+  );
+  return response;
+};
+
+export const setEventAnswer = ({ iqryNo, rpyCntn }) => {
+  const response = Api.post(`/viewapi/mngr/site/v1/even/rpy`, {
+    iqryNo,
+    rpyCntn,
+  });
+  return response;
+};
