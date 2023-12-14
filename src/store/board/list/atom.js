c8e0805ef9a64af1e487c0bcb82a73dd059d42a4
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-14 20:07:06 +0900

    add : boardListAtom

---
+++ src/store/board/list/atom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const BoardListAtom = atom({
+  key: "BoardListAtom",
+  default: [],
+});
+
+export const BoardDetailListAtom = atom({
+  key: "BoardListAtom",
+  default: [],
+});
