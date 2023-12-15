b83685801ff6579d445717f12598d8cf5870763e
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-15 20:34:24 +0900

    fix : setting join button Error

--- src/components/radio/slide_toggle_btn.js
+++ src/components/radio/slide_toggle_btn.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-
-const Component = ({state, id }) => {
-
-	return(
-		<div className={`toggle-container flex_ ${state}`}>
-			<input 
-				type="checkbox" 
-				id={id} 
-			/>
-			<label  for={id} />
-		</div>
-	)
+const Component = ({ state, id, setState, checked }) => {
+  return (
+    <div className={`toggle-container flex_ ${state}`}>
+      <input
+        type="checkbox"
+        id={id}
+        checked={checked}
+        onChange={(e) => (e.target.checked ? setState("I") : setState("N"))}
+      />
+      <label for={id} />
+    </div>
+  );
 };
 
 export default Component;
