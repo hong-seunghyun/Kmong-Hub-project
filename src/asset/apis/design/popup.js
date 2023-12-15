d8133dafcb6a0eee60c6ec1e51b43bfbb65b1b7e
이재잰 <HINU0622@gmail.com>
2023-12-15 10:34:57 +0900

    검색 기능 구현

--- src/asset/apis/design/popup.js
+++ src/asset/apis/design/popup.js
@@ -1,8 +1,20 @@
 import { Api } from "../../config/Api";
 
 // post(팝업 불러오기)
-export const getPopUp = (currentPage, limit, searchType, searchValue) => {
-  const response = Api.get(`/viewapi/mngr/site/v2/signin`, {
+export const getPopUp = (currentPage, limit, searchValue) => {
+  const response = Api.get(`/viewapi/mngr/site/v1/popu`, {
+    params: {
+      currentPage: currentPage,
+      limit: limit,
+      searchValue: searchValue
+    }
+  });
+  return response;
+}
+
+// post(팝업 불러오기)
+export const getPopUpActive = (currentPage, limit, searchType, searchValue) => {
+  const response = Api.get(`/viewapi/mngr/site/v1/popu`, {
     params: {
       currentPage: currentPage,
       limit: limit,
