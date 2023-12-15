70560d84a659dcb7fa20214ed1c5e9d05f6637ad
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-15 15:39:52 +0900

    add : contents atom

---
+++ src/store/contents/contents/atom.js
@@ -0,0 +1,81 @@
+import { atom } from "recoil";
+
+export const TTFTitleAtom = atom({
+  key: "TTFTitle",
+  default: "",
+});
+
+export const TTFTitleExpAtom = atom({
+  key: "TTFTitleExp",
+  default: "Y",
+});
+
+export const TTFIntroduceAtom = atom({
+  key: "TTFIntroduce",
+  default: "",
+});
+
+export const TTFIntroduceExpAtom = atom({
+  key: "TTFIntroduceExp",
+  default: "Y",
+});
+
+export const NewsTitleAtom = atom({
+  key: "NewsTitle",
+  default: "",
+});
+
+export const NewsTitleExpAtom = atom({
+  key: "NewsTitleExp",
+  default: "Y",
+});
+
+export const NewsIntroduceAtom = atom({
+  key: "NewsIntroduce",
+  default: "",
+});
+
+export const NewsIntroduceExpAtom = atom({
+  key: "NewsIntroduceExp",
+  default: "Y",
+});
+
+export const SupportTitleAtom = atom({
+  key: "SupportTitle",
+  default: "",
+});
+
+export const SupportTitleExpAtom = atom({
+  key: "SupportTitleExp",
+  default: "Y",
+});
+
+export const SupportIntroduceAtom = atom({
+  key: "SupportIntroduce",
+  default: "",
+});
+
+export const SupportIntroduceExpAtom = atom({
+  key: "SupportIntroduceExp",
+  default: "Y",
+});
+
+export const EventTitleAtom = atom({
+  key: "EventTitle",
+  default: "",
+});
+
+export const EventTitleExpAtom = atom({
+  key: "EventTitleExp",
+  default: "Y",
+});
+
+export const EventIntroduceAtom = atom({
+  key: "EventIntroduce",
+  default: "",
+});
+
+export const EventIntroduceExpAtom = atom({
+  key: "EventIntroduceExp",
+  default: "Y",
+});
