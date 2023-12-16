7ac3b6985fdec5e6c65b31adc69cfa4badb5091a
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-16 13:33:48 +0900

    feat : server connect

--- src/containers/inquiry/technology.js
+++ src/containers/inquiry/technology.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ButtonM from "/src/components/buttons/button_primary_m";
 import DropDownMenu from "/src/components/dropsMenu/drops_company_management_menu";
 import SearchBar from "/src/components/searchBar/search_bar_company_management_menu";
@@ -6,169 +6,69 @@ import TableHead from "/src/components/table/inquiry_technology_table_head";
 import TableCell from "/src/components/table/inquiry_technology_table_cell";
 import Pagnation from "/src/components/pagnation/pagnation";
 import Link from "next/link";
+import { getTechTransferList } from "../../asset/apis/inquiry/techTransferApis";
 
 const Component = () => {
-	return(
-		<div className="page-wrap">
-			<div className="inquiry- ">
-				<div className="transaction-01">
+  const [techList, setTechList] = useState([]);
 
-					<div className="head-wrap flex_">
-						<h1 className="display-5-B">
-							기술이전 문의 관리
-						</h1>
-					</div>
+  const getValue = async () => {
+    await getTechTransferList({ currentIdx: 1 })
+      .then((e) => setTechList([...e.data.data]))
+      .catch((e) => console.log(e));
+  };
 
-					<div>
-						<div className="flex_ search-wrap">
-							<DropDownMenu />
-							<SearchBar />
-						</div>
+  useEffect(() => {
+    getValue();
+  }, []);
 
-						<div className="table-container">
-							<TableHead
-								headChoice="번호"
-								headCategory="상태"
-								headTitle="제목"
-								headWriter="작성자"
-								headDate="등록일"
-								headEtc="관리"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-1"
-								labelColor="txt-white"
-								label="답변 전"
-								link="/inquiry/technology_detail"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-1"
-								labelColor="txt-white"
-								label="답변 전"
-								link="/inquiry/technology_detail"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-5"
-								labelColor="txt-violet-1"
-								label="답변 완료"
-								link="/inquiry/technology_detail_2"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-5"
-								labelColor="txt-violet-1"
-								label="답변 완료"
-								link="/inquiry/technology_detail_2"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-1"
-								labelColor="txt-white"
-								label="답변 전"
-								link="/inquiry/technology_detail"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-1"
-								labelColor="txt-white"
-								label="답변 전"
-								link="/inquiry/technology_detail"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-5"
-								labelColor="txt-violet-1"
-								label="답변 완료"
-								link="/inquiry/technology_detail_2"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-5"
-								labelColor="txt-violet-1"
-								label="답변 완료"
-								link="/inquiry/technology_detail_2"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-1"
-								labelColor="txt-white"
-								label="답변 전"
-								link="/inquiry/technology_detail"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-1"
-								labelColor="txt-white"
-								label="답변 전"
-								link="/inquiry/technology_detail"
-							/>
-							<Pagnation size="regular"/>
-						</div>
-					</div>
-				</div>
-				
-			</div>
-		</div>
-	)
+  return (
+    <div className="page-wrap">
+      <div className="inquiry- ">
+        <div className="transaction-01">
+          <div className="head-wrap flex_">
+            <h1 className="display-5-B">기술이전 문의 관리</h1>
+          </div>
+
+          <div>
+            <div className="flex_ search-wrap">
+              <DropDownMenu />
+              <SearchBar />
+            </div>
+
+            <div className="table-container">
+              <TableHead
+                headChoice="번호"
+                headCategory="상태"
+                headTitle="제목"
+                headWriter="작성자"
+                headDate="등록일"
+                headEtc="관리"
+              />
+              {techList.map((e, idx) => (
+                <TableCell
+                  choice="number"
+                  number={idx + 1}
+                  img={e.thumAddr}
+                  title={e.titleNm}
+                  writer={e.wrterMbrNo}
+                  date={e.createdDate}
+                  labelBg={e.prcsYn === "N" ? "bg-white" : "bg-violet-1"}
+                  labelColor={e.prcsYn === "N" ? "txt-violet-1" : "txt-white"}
+                  label={e.prcsYn === "Y" ? "답변 완료" : "답변 전"}
+                  link={
+                    e.prcsYn === "Y"
+                      ? `/inquiry/technology_detail_complete/${e.iqryNo}`
+                      : `/inquiry/technology_detail/${e.iqryNo}`
+                  }
+                />
+              ))}
+              <Pagnation size="regular" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Component;
