cda9613ff3dcdea10760cde80da3f2a6258a3d1e
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-16 22:26:38 +0900

    feat : support connect server

--- src/asset/apis/inquiry/supportApis.js
+++ src/asset/apis/inquiry/supportApis.js
@@ -1,4 +1,4 @@
-import { Api } from "../config/Api";
+import { Api } from "../../config/Api";
 
 export const getSupportList = ({ currentIdx, searchType, search }) => {
   const response = Api.get(
--- src/containers/inquiry/support.js
+++ src/containers/inquiry/support.js
@@ -1,175 +1,78 @@
-import React from "react";
-import ButtonM from "/src/components/buttons/button_primary_m";
+import React, { useEffect, useState } from "react";
 import DropDownMenu from "/src/components/dropsMenu/drops_company_management_menu";
 import SearchBar from "/src/components/searchBar/search_bar_company_management_menu";
 import TableHead from "/src/components/table/inquiry_technology_table_head";
 import TableCell from "/src/components/table/inquiry_technology_table_cell";
 import Pagnation from "/src/components/pagnation/pagnation";
-import Link from "next/link";
+import { getSupportList } from "../../asset/apis/inquiry/supportApis";
 
 const Component = () => {
-	return(
-		<div className="page-wrap">
-			<div className="inquiry- ">
-				<div className="transaction-01">
+  const [supportList, setSupportList] = useState([]);
+  const [pageIdx, setPageIdx] = useState(1);
 
-					<div className="head-wrap flex_">
-						<h1 className="display-5-B">
-							지원사업 문의 관리
-						</h1>
-					</div>
+  const getValue = async () => {
+    await getSupportList({ currentIdx: pageIdx })
+      .then((e) => setSupportList([...e.data.data]))
+      .catch((e) => console.log(e));
+  };
 
-					<div>
-						<div className="flex_ search-wrap">
-							<DropDownMenu />
-							<SearchBar />
-						</div>
+  useEffect(() => {
+    getValue();
+  }, [pageIdx]);
 
-						<div className="table-container">
-							<TableHead
-								headChoice="번호"
-								headCategory="상태"
-								headTitle="제목"
-								headWriter="작성자"
-								headDate="등록일"
-								headEtc="관리"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-1"
-								labelColor="txt-white"
-								label="답변 전"
-								link="/inquiry/support_detail"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-1"
-								labelColor="txt-white"
-								label="답변 전"
-								link="/inquiry/support_detail"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-5"
-								labelColor="txt-violet-1"
-								label="답변 완료"
-								link="/inquiry/support_detail_2"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-5"
-								labelColor="txt-violet-1"
-								label="답변 완료"
-								link="/inquiry/support_detail_2"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-1"
-								labelColor="txt-white"
-								label="답변 전"
-								link="/inquiry/support_detail"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-1"
-								labelColor="txt-white"
-								label="답변 전"
-								link="/inquiry/support_detail"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-5"
-								labelColor="txt-violet-1"
-								label="답변 완료"
-								link="/inquiry/support_detail_2"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-5"
-								labelColor="txt-violet-1"
-								label="답변 완료"
-								link="/inquiry/support_detail_2"
-								
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-1"
-								labelColor="txt-white"
-								label="답변 전"
-								link="/inquiry/support_detail"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								img="/images/sample-img.png"
-								title="문의 제목"
-								writer="미나 마수드"
-								date="YYYY.MM.DD"
-								labelBg="bg-violet-1"
-								labelColor="txt-white"
-								label="답변 전"
-								link="/inquiry/support_detail"
-							/>
-							<Pagnation size="regular"/>
-						</div>
-					</div>
-				</div>
-				
-			</div>
-		</div>
-	)
+  return (
+    <div className="page-wrap">
+      <div className="inquiry- ">
+        <div className="transaction-01">
+          <div className="head-wrap flex_">
+            <h1 className="display-5-B">지원사업 문의 관리</h1>
+          </div>
+
+          <div>
+            <div className="flex_ search-wrap">
+              <DropDownMenu />
+              <SearchBar />
+            </div>
+
+            <div className="table-container">
+              <TableHead
+                headChoice="번호"
+                headCategory="상태"
+                headTitle="제목"
+                headWriter="작성자"
+                headDate="등록일"
+                headEtc="관리"
+              />
+              {supportList &&
+                supportList.map((e, idx) => (
+                  <TableCell
+                    choice="number"
+                    number={idx + 1}
+                    img={e.thumAddr}
+                    title={e.titleNm}
+                    writer={e.wrterMbrNo}
+                    date={e.createdDate}
+                    labelBg={e.prcsYn === "Y" ? "bg-violet-1" : "bg-white"}
+                    labelColor={e.prcsYn === "Y" ? "txt-white" : "txt-violet-1"}
+                    label={e.prcsYn === "Y" ? "답변 완료" : "답변 전"}
+                    link={
+                      e.prcsYn === "Y"
+                        ? `/inquiry/support_detail_complete/${e.iqryNo}`
+                        : `/inquiry/support_detail/${e.iqryNo}`
+                    }
+                  />
+                ))}
+              <Pagnation
+                size="regular"
+                pageIdx={pageIdx}
+                setPageIdx={setPageIdx}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Component;
