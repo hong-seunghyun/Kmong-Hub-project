b590a72a99360a0361e448fbede0b1e8a5b4e838
이재잰 <HINU0622@gmail.com>
2023-12-16 16:43:20 +0900

    Popup 검색 Api 수정

--- src/asset/apis/design/popup.js
+++ src/asset/apis/design/popup.js
@@ -3,10 +3,16 @@ import { Api } from "../../config/Api";
 // get(팝업 불러오기)
 export const getPopUp = (currentPage, limit, searchValue) => {
   const response = Api.get(`/viewapi/mngr/site/v1/popu`, {
-    params: {
+    params: searchValue ? {
       currentPage: currentPage,
       limit: limit,
       searchValue: searchValue
+    } : {
+      currentPage: currentPage,
+      limit: limit
+    },
+    headers: {
+      Authorization: `Bearer ${window.localStorage.getItem('accessToken')}`
     }
   });
   return response;
