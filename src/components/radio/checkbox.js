11ac3cb6596566772619cabe97889be46f3901fa
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-17 22:56:31 +0900

    fix : check box

--- src/components/radio/checkbox.js
+++ src/components/radio/checkbox.js
@@ -1,18 +1,25 @@
 import React from "react";
 
 const Component = (props) => {
+  let checked = props.checked;
+  const onchange = () => {
+    checked = !checked;
+    if (props.setCheckState) props.setCheckState(checked);
+    if (props.setCheckFunc) props.setCheckFunc();
+  };
 
-	let checked = props.checked;
-	const onchange = () => {
-		checked = !checked;
-		props.setCheckState(checked);
-	}
-
-	return(
-		<div className={`check-container flex_ body-3-R ${props.state} ${props.size}`}>
-			<input type="checkbox" id={props.id} onChange={onchange} checked={props.checked}/>
-			<label for={props.id}>{props.label}</label>
-		</div>
-	)
+  return (
+    <div
+      className={`check-container flex_ body-3-R ${props.state} ${props.size}`}
+    >
+      <input
+        type="checkbox"
+        id={props.id}
+        onChange={onchange}
+        checked={props.checked}
+      />
+      <label htmlFor={props.id}>{props.label}</label>
+    </div>
+  );
 };
 export default Component;
