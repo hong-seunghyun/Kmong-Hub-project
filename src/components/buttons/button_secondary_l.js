f8a03deb982b083f6fc82758c215ad614eff292c
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-17 23:21:51 +0900

    fix : button onclick func

--- src/components/buttons/button_secondary_l.js
+++ src/components/buttons/button_secondary_l.js
@@ -1,28 +1,30 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Icon from "/src/components/icon/icon.tsx";
 
-
 const Component = (props) => {
-	const [isButton, setButton ] = useState(false);
+  const [isButton, setButton] = useState(false);
 
-	const onClickActive = () => {
-		props.onclick();
-		setButton(!isButton);
-	}
+  const onClickActive = () => {
+    if (props.onclick) props.onclick();
+    setButton(!isButton);
+  };
 
-	const ButtonSecondaryL = () => {
-		return(
-			<button className={`btn_ radius-8 ac-purple btn-l body-2-R ${isButton ? "pressed" : ""} ${props.state}`} onClick={onClickActive} >
-				<span className="flex_">
-					<Icon size={11.6} color="#fff" stroke="none" icon="plus" />
-						{props.text} 
-						<Icon size={13} color="#fff" stroke="none" icon="chevronRight" />
-				</span>
-			</button>
-		)
-	}
-	return(
-			<ButtonSecondaryL />
-	)
-}
+  const ButtonSecondaryL = () => {
+    return (
+      <button
+        className={`btn_ radius-8 ac-purple btn-l body-2-R ${
+          isButton ? "pressed" : ""
+        } ${props.state}`}
+        onClick={onClickActive}
+      >
+        <span className="flex_">
+          <Icon size={11.6} color="#fff" stroke="none" icon="plus" />
+          {props.text}
+          <Icon size={13} color="#fff" stroke="none" icon="chevronRight" />
+        </span>
+      </button>
+    );
+  };
+  return <ButtonSecondaryL />;
+};
 export default Component;
