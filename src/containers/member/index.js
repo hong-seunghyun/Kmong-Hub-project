84bae8666b40e15fc9526169408bc45ddd90f249
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-17 17:11:22 +0900

    fix : add member detail page query

--- src/containers/member/index.js
+++ src/containers/member/index.js
@@ -8,128 +8,124 @@ import Pagnation from "/src/components/pagnation/pagnation";
 import Link from "next/link";
 
 const Component = () => {
-	return(
-		<div className="page-wrap">
-			<div className="member- ">
+  return (
+    <div className="page-wrap">
+      <div className="member- ">
+        <div className="head-wrap flex_">
+          <h1 className="display-5-B">회원 관리</h1>
+        </div>
 
-					<div className="head-wrap flex_">
-						<h1 className="display-5-B">
-							회원 관리
-						</h1>
-					</div>
+        <div>
+          <div className="flex_ search-wrap">
+            <DropDownMenu />
+            <SearchBar />
+          </div>
 
-					<div>
-						<div className="flex_ search-wrap">
-							<DropDownMenu />
-							<SearchBar />
-						</div>
-
-						<div className="table-container">
-							<TableHead
-								headChoice="번호"
-								headTitle="이름"
-								headEmail="이메일"
-								headDate="가입일"
-								headAccess="최근 접속일"
-								headEtc="관리"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								title="[이름]"
-								email="kotech@kotech.co.kr"
-								date="YYYY.MM.DD"
-								access="YYYY.MM.DD HH:MM:SS"
-								listLink="/member/member_detail"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								title="[이름]"
-								email="kotech@kotech.co.kr"
-								date="YYYY.MM.DD"
-								access="YYYY.MM.DD HH:MM:SS"
-								listLink="/member/member_detail"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								title="[이름]"
-								email="kotech@kotech.co.kr"
-								date="YYYY.MM.DD"
-								access="YYYY.MM.DD HH:MM:SS"
-								listLink="/member/member_detail"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								title="[이름]"
-								email="kotech@kotech.co.kr"
-								date="YYYY.MM.DD"
-								access="YYYY.MM.DD HH:MM:SS"
-								listLink="/member/member_detail"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								title="[이름]"
-								email="kotech@kotech.co.kr"
-								date="YYYY.MM.DD"
-								access="YYYY.MM.DD HH:MM:SS"
-								listLink="/member/member_detail"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								title="[이름]"
-								email="kotech@kotech.co.kr"
-								date="YYYY.MM.DD"
-								access="YYYY.MM.DD HH:MM:SS"
-								listLink="/member/member_detail"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								title="[이름]"
-								email="kotech@kotech.co.kr"
-								date="YYYY.MM.DD"
-								access="YYYY.MM.DD HH:MM:SS"
-								listLink="/member/member_detail"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								title="[이름]"
-								email="kotech@kotech.co.kr"
-								date="YYYY.MM.DD"
-								access="YYYY.MM.DD HH:MM:SS"
-								listLink="/member/member_detail"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								title="[이름]"
-								email="kotech@kotech.co.kr"
-								date="YYYY.MM.DD"
-								access="YYYY.MM.DD HH:MM:SS"
-								listLink="/member/member_detail"
-							/>
-							<TableCell 
-								choice="number"
-								number="1"
-								title="[이름]"
-								email="kotech@kotech.co.kr"
-								date="YYYY.MM.DD"
-								access="YYYY.MM.DD HH:MM:SS"
-								listLink="/member/member_detail"
-							/>
-							<Pagnation size="regular"/>
-						</div>
-					</div>
-				</div>
-				
-			</div>
-	)
+          <div className="table-container">
+            <TableHead
+              headChoice="번호"
+              headTitle="이름"
+              headEmail="이메일"
+              headDate="가입일"
+              headAccess="최근 접속일"
+              headEtc="관리"
+            />
+            <TableCell
+              choice="number"
+              number="1"
+              title="[이름]"
+              email="kotech@kotech.co.kr"
+              date="YYYY.MM.DD"
+              access="YYYY.MM.DD HH:MM:SS"
+              listLink="/member/member_detail"
+            />
+            <TableCell
+              choice="number"
+              number="1"
+              title="[이름]"
+              email="kotech@kotech.co.kr"
+              date="YYYY.MM.DD"
+              access="YYYY.MM.DD HH:MM:SS"
+              listLink="/member/member_detail"
+            />
+            <TableCell
+              choice="number"
+              number="1"
+              title="[이름]"
+              email="kotech@kotech.co.kr"
+              date="YYYY.MM.DD"
+              access="YYYY.MM.DD HH:MM:SS"
+              listLink="/member/member_detail"
+            />
+            <TableCell
+              choice="number"
+              number="1"
+              title="[이름]"
+              email="kotech@kotech.co.kr"
+              date="YYYY.MM.DD"
+              access="YYYY.MM.DD HH:MM:SS"
+              listLink="/member/member_detail"
+            />
+            <TableCell
+              choice="number"
+              number="1"
+              title="[이름]"
+              email="kotech@kotech.co.kr"
+              date="YYYY.MM.DD"
+              access="YYYY.MM.DD HH:MM:SS"
+              listLink="/member/member_detail"
+            />
+            <TableCell
+              choice="number"
+              number="1"
+              title="[이름]"
+              email="kotech@kotech.co.kr"
+              date="YYYY.MM.DD"
+              access="YYYY.MM.DD HH:MM:SS"
+              listLink="/member/member_detail"
+            />
+            <TableCell
+              choice="number"
+              number="1"
+              title="[이름]"
+              email="kotech@kotech.co.kr"
+              date="YYYY.MM.DD"
+              access="YYYY.MM.DD HH:MM:SS"
+              listLink="/member/member_detail"
+            />
+            <TableCell
+              choice="number"
+              number="1"
+              title="[이름]"
+              email="kotech@kotech.co.kr"
+              date="YYYY.MM.DD"
+              access="YYYY.MM.DD HH:MM:SS"
+              listLink="/member/member_detail"
+            />
+            <TableCell
+              choice="number"
+              number="1"
+              title="[이름]"
+              email="kotech@kotech.co.kr"
+              date="YYYY.MM.DD"
+              access="YYYY.MM.DD HH:MM:SS"
+              listLink="/member/member_detail"
+            />
+            <TableCell
+              choice="number"
+              number="1"
+              title="[이름]"
+              email="kotech@kotech.co.kr"
+              date="YYYY.MM.DD"
+              access="YYYY.MM.DD HH:MM:SS"
+              listLink="/member/member_detail"
+            />
+            <Pagnation size="regular" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Component;
