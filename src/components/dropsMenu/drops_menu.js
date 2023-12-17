df4d1acc3c43dee6401a9632c14e2667cb656a92
이재잰 <HINU0622@gmail.com>
2023-12-17 14:09:55 +0900

    동적 코드로 변경

--- src/components/dropsMenu/drops_menu.js
+++ src/components/dropsMenu/drops_menu.js
@@ -1,58 +1,16 @@
 import React, {useState} from "react";
 import Icon from "/src/components/icon/icon.tsx";
 
-
-const dummy = [
-	{
-		id: 1,
-		title : "TestDate 1",
-	},
-	{
-		id: 2,
-		title : "TestDate 2",
-	},
-	{
-		id: 3,
-		title : "TestDate 3",
-	},
-	{
-		id: 4,
-		title : "TestDate 4",
-	},
-	{
-		id: 5,
-		title : "d",
-	},
-	{
-		id: 6,
-		title : "c",
-	},
-	{
-		id: 7,
-		title : "b",
-	},
-	{
-		id: 8,
-		title : "a",
-	},
-	{
-		id: 9,
-		title : "ㄱㄴㄷㄹ",
-	},
-	{
-		id: 10,
-		title : "ㅁㅂ ㅅㅇ",
-	},
-	{
-		id: 11,
-		title : "ㅈㅋㅍㅎ",
-	},
-]
-
 const Component = (props) => {
 	const [ search, setSearch ] = useState("");
+
+	const [ toggle, setToggle ] = useState(false);
+
+	const dummy = props.datas;
+
 	const onChange = (e) => {
 		setSearch(e.target.value);
+		setToggle(true);
 	}
 
 	const filterTitle = dummy.filter((p) => {
@@ -66,16 +24,19 @@ const Component = (props) => {
 					type="text" 
 					placeholder={props.placeholder}
 					value={search}
-					onChange={onChange}	
+					onChange={onChange}
 				/>
 				<span className="icon_serach">
 					<Icon size={9} color="#464749" stroke="" icon="chevronDown" />
 				</span>
 			</div>
-			<div className="wrap radius-8 border-gray-4">
+			<div className="wrap radius-8 border-gray-4" style={{display: toggle ? "block" : "none"}}>
 				<div className="flex_ result-search-box  body-3-R ">
 					{
-						filterTitle.map(dummy => <span>{dummy.title}</span>)
+						filterTitle.map(dummy => <span onClick={() => {
+							setSearch(dummy.title);
+							setToggle(false);
+						}}>{dummy.title}</span>)
 					}
 				</div>
 			</div>
