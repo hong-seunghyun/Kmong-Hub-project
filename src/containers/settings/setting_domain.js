3f77f90007cc70504c0b3c3759c576e2e2ea6e27
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-19 07:23:57 +0900

    feat : server connect

--- src/containers/settings/setting_domain.js
+++ src/containers/settings/setting_domain.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Tab from "/src/components/tabs/settings_tab_fixed";
 import Input from "/src/components/textFields/textInput.tsx";
 import OutlineBtn from "/src/components/buttons/button_outline_l";
@@ -7,20 +7,61 @@ import TextButtonUnderlineS from "/src/components/buttons/text_button_underline_
 import { useRecoilState, useSetRecoilState } from "recoil";
 import {
   SettingBsicDmiAddr,
+  SettingBslNo,
+  SettingCbdAddr,
+  SettingCbdNm,
   SettingIdvdDmiAddr,
+  SettingOmbRptNo,
+  SettingPtpPicEmailAddr,
+  SettingPtpPicNm,
+  SettingPtpPicPhcNo,
+  SettingRpsvNm,
+  SettingSiteInfoId,
 } from "../../store/setting/business/atom";
+import { getSiteBsl } from "../../asset/apis/siteApis";
 
 const Component = () => {
   const [subTab, setSubTab] = useState(0);
   const [activeSubTab, setActiveSubTab] = useState(0);
-
   const setBsicDmiAddr = useSetRecoilState(SettingBsicDmiAddr);
   const setIdvdDmiAddr = useSetRecoilState(SettingIdvdDmiAddr);
+  const setBslNo = useSetRecoilState(SettingBslNo);
+  const setCbdAddr = useSetRecoilState(SettingCbdAddr);
+  const setCbdNm = useSetRecoilState(SettingCbdNm);
+  const setOmbRptNo = useSetRecoilState(SettingOmbRptNo);
+  const setRpsvNm = useSetRecoilState(SettingRpsvNm);
+  const setSiteInfoId = useSetRecoilState(SettingSiteInfoId);
+  const setPtpPicEmailAddr = useSetRecoilState(SettingPtpPicEmailAddr);
+  const setPtpPicNm = useSetRecoilState(SettingPtpPicNm);
+  const setPtpPicPhcNo = useSetRecoilState(SettingPtpPicPhcNo);
+
+  const getValue = async () => {
+    await getSiteBsl()
+      .then((e) => {
+        if (e.data.data.bsicDmiAddr) setBsicDmiAddr(e.data.data.bsicDmiAddr);
+        if (e.data.data.bslNo) setBslNo(e.data.data.bslNo);
+        if (e.data.data.cbdAddr) setCbdAddr(e.data.data.cbdAddr);
+        if (e.data.data.cbdNm) setCbdNm(e.data.data.cbdNm);
+        if (e.data.data.idvdDmiAddr) setIdvdDmiAddr(e.data.data.idvdDmiAddr);
+        if (e.data.data.ombRptNo) setOmbRptNo(e.data.data.ombRptNo);
+        if (e.data.data.ptpPicEmailAddr)
+          setPtpPicEmailAddr(e.data.data.ptpPicEmailAddr);
+        if (e.data.data.ptpPicNm) setPtpPicNm(e.data.data.ptpPicNm);
+        if (e.data.data.ptpPicPhcNo) setPtpPicPhcNo(e.data.data.ptpPicPhcNo);
+        if (e.data.data.rpsvNm) setRpsvNm(e.data.data.rpsvNm);
+        if (e.data.data.siteInfoId) setSiteInfoId(e.data.data.siteInfoId);
+      })
+      .catch((e) => console.log(e));
+  };
+
   const setInitValue = () => {
     setBsicDmiAddr("");
-
     setIdvdDmiAddr("");
   };
+
+  useEffect(() => {
+    getValue();
+  }, []);
 
   const TabContents = () => {
     const [bsicDmiAddr, setBsicDmiAddr] = useRecoilState(SettingBsicDmiAddr);
