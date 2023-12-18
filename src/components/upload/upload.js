8e5261639bcc9756548144442d581b53c2f22b10
이재잰 <HINU0622@gmail.com>
2023-12-18 12:12:06 +0900

    파일 업로드 빠진 부분 수정

--- src/components/upload/upload.js
+++ src/components/upload/upload.js
@@ -6,6 +6,7 @@ import { sleep } from "../../util/sleep";
 
 const Component = (props) => {
   const [fileSize, setFileSize] = useState(null);
+  const [fileName, setFileName] = useState('');
   const [process, setProcess] = useState(20);
 
   const [state, setState] = useState(null);
@@ -26,20 +27,25 @@ const Component = (props) => {
     const file = e.target.files[0];
     props.setFileState(file);
     setFileSize(e.target.files[0].size);
+    setFileName(e.target.files[0].name);
     const MAX_SIZE = 100 * 1024 * 1024;
 
+    const ext = e.target.files[0].name.split('.').pop().toLowerCase();
+    console.log(ext);
+
     if (fileSize > MAX_SIZE) {
-      alert("이미지가 너무 무겁습니다!");
+      alert("파일이 너무 무겁습니다!");
     }
 
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
-        getImageSize(reader.result);
-        if (imgWidth + imgHeight > 1600) {
-          alert("이미지 크기가 너무 큽니다!");
+        if(ext === 'jpg' || ext === 'jpeg' || ext === 'png') {
+          getImageSize();
+          if (imgWidth + imgHeight > 1600) {
+            alert("이미지 크기가 너무 큽니다!");
+          }
         }
-
         setUrlState(reader.result);
       };
       reader.readAsDataURL(file);
@@ -56,16 +62,14 @@ const Component = (props) => {
 
       console.log(e.target.files[0].name);
 
-      const ext = e.target.files[0].name.split('.').pop().toLowerCase();
-      console.log(ext);
       if(ext === 'jpg' || ext === 'jpeg' || ext === 'png') {
         setState("done");
         setType("preview");
       } else if(ext === 'hwp' || ext === 'docx' || ext === 'pdf') {
         setState('done');
-        setType('technology')
+        setType('normal')
       } else {
-        console.log('지원되지 않는 파일 형식입니다.');
+        alert('지원되지 않는 파일 형식입니다.');
         setState('default');
         setType(props.type)
       }
@@ -88,6 +92,12 @@ const Component = (props) => {
   const handleSpanClick = () => {
     fileInputRef.current.click();
   };
+
+  const changeOrigin = () => {
+    setState('default');
+    setType(props.type);
+    setUrlState(null);
+  }
 
   return (
     <div
@@ -160,15 +170,15 @@ const Component = (props) => {
 
         <div className="done-normal-text-box flex_">
           <div className="text-wrap">
-            <h6 className="body-3-B">파일명.확장자</h6>
-            <p className="caption-R">파일크기 mb</p>
+            <h6 className="body-3-B">{fileName}</h6>
+            <p className="caption-R">{fileSize / 100 / 1024 / 1024} mb</p>
           </div>
-          <Icon icon="cancel" size={9} color="#464749" stroke="" />
+          <Icon icon="cancel" size={9} color="#464749" stroke="" onClick={changeOrigin}/>
         </div>
 
         <div className="done-preview-text-box flex_">
           <div className="text-wrap">
-            <h6 className="body-3-B">파일명.확장자</h6>
+            <h6 className="body-3-B">{fileName}</h6>
             <p className="caption-R">파일크기 {fileSize / 1024 / 1024}mb</p>
           </div>
           <div className="flex_">
