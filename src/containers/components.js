8033fa62f9b09b93e1ecaf62e60a1389f6b03b25
Hongseunghyun <teddy@another-space.co.kr>
2023-12-18 12:28:14 +0900

    네비게이션 합체 오류

--- src/containers/components.js
+++ src/containers/components.js
@@ -2,7 +2,7 @@
 // HSD Project index.js
 import React from "react";
 
-import NavigationSide from "/src/components/navigations/navigation_side";
+
 import NavigationTopLogin from "/src/components/navigations/navigation_top_login";
 import NavigationTop from "/src/components/navigations/navigation_top";
 import ButtonCta from "/src/components/buttons/button_cta";
@@ -68,7 +68,7 @@ const Component = () => {
       <div style={{ margin: "20px" }}>
         <h1>Navigation Side</h1>
 
-        <NavigationSide />
+        
         <div style={{ margin: "100vh" }} />
         <hr />
 
