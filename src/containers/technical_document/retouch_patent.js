a8a7d3eb283f63b4426d0ba1309b8311e4dd4e36
이재잰 <HINU0622@gmail.com>
2023-12-18 20:32:39 +0900

    기술 문서 관리 특허 수정 페이지

--- src/containers/technical_document/retouch_patent.js
+++ src/containers/technical_document/retouch_patent.js
@@ -10,8 +10,93 @@ import DatePicker from "/src/components/date/date-picker-single"
 import Upload from "/src/components/upload/upload"
 import CheckBox from "/src/components/radio/checkbox"
 import { Editor } from "@tinymce/tinymce-react";
+import { useLayoutEffect } from "react";
+import { getTechDetails } from "../../asset/apis/tech";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const drop_datas = [
+	{
+		id: "P",
+		title: '특허'
+	},
+	{
+		id: "T",
+		title: '논문'
+	},
+	{
+		id: "R",
+		title: '보고서'
+	}
+];
+
+const send_datas = [
+	{
+		id: 'O',
+		title: '출원 중'
+	},
+	{
+		id: 'O',
+		title: '미공개 상태'
+	},
+	{
+		id: 'O',
+		title: '공개 상태'
+	},
+	{
+		id: 'O',
+		title: '등록 완료'
+	},
+	{
+		id: "O",
+		title: '취하 상태'
+	},
+	{
+		id: 'O',
+		title: '거절 상태'
+	}
+];
 
 const Component = () => {
+
+	const router = useRouter();
+	const [ data, setData ] = useState([]);
+
+	const [ typeCd, setTypeCd ] = useState('P');
+  const [ tcqNm, setTcqNm ] = useState('');
+  const [ rsacUcmdCd, setRsacUcmdCd ] = useState('');
+  const [ apyNo, setApyNo ] = useState('');
+  const [ apyAd, setApyAd ] = useState('');
+  const [ statCd, setStatCd ] = useState('');
+  const [ rgstNo, setRgstNo ] = useState('');
+  const [ ivtNm, setIvtNm ] = useState('');
+  const [ ipcVal, setIpcVal ] = useState('');
+  const [ cpcVal, setCpcVal ] = useState('');
+  const [ piuaYn, setPiuaYn ] = useState('');
+  
+  const [ orgn, setOrgn ] = useState('');
+  const [ file, setFile ] = useState('');
+
+	useLayoutEffect(() => {
+		if(!router.isReady) return;
+		const no = router.query.no;
+		getTechDetails(no).then(res => {
+			console.log(res.data);
+			setData(res.data.data);
+			initData(res.data.data);
+		}).catch(err => {
+			console.log(err);
+		});
+	},[router.isReady]);
+
+	const initData = (data) => {
+		setTcqNm(data.tcqNm);
+		setRsacUcmdCd(data.rsacUcmdCd);
+		setOrgn(data.orgnNm);
+		setApyNo(data.apyNo);
+		setRgstNo(data.rgstNo);
+	}
+
 	return(
 		<div className="page-wrap">
 			<div className="board- board-add document- technical-document-add">
@@ -26,7 +111,7 @@ const Component = () => {
 					<Badge value="1"/>
 						유형 선택
 				</div>
-				<DropsMenu />
+				<DropsMenu datas={drop_datas} default={typeCd == 'P' ? "특허" : typeCd == 'T' ? '논문' : '보고서'} setState={setTypeCd}/>
 
 				<div className="content- content-2">
 					<div className="sub-title body-2-B flex_">
@@ -34,29 +119,29 @@ const Component = () => {
 							기본 정보
 					</div>
 					<div className="box-">
-						<Input labelText="기술명" placeholder="기술명을 입력해 주세요." helperTextResult="none" iconState="false"/>
+						<Input labelText="기술명" placeholder="기술명을 입력해 주세요." helperTextResult="none" iconState="false" state={tcqNm} setState={setTcqNm}/>
 					</div>
 
 					<div className="flex_ input-search box-">
-						<Input labelText="소속" placeholder="소속을 검색해 주세요." valueType="" helperTextResult="none" iconState="false"/>
+						<Input labelText="소속" placeholder="소속을 검색해 주세요." valueType="" helperTextResult="none" iconState="false" state={orgn} setState={setOrgn}/>
 						<Icon icon="search" size={16} stroke="none" color="#574AFF" /> 
 					</div>
 
 					<div className="box-">
-						<Input importState="none" labelText="출원번호" placeholder="출원번호를 입력해 주세요." helperTextResult="none" iconState="false"/>
+						<Input importState="none" labelText="출원번호" placeholder="출원번호를 입력해 주세요." helperTextResult="none" iconState="false" state={apyNo} setState={setApyNo}/>
 					</div>
 
 					<div className="box-">
-						<Input importState="none" labelText="등록번호" placeholder="등록번호 입력해 주세요." helperTextResult="none" iconState="false"/>
+						<Input importState="none" labelText="등록번호" placeholder="등록번호 입력해 주세요." helperTextResult="none" iconState="false" state={rgstNo} setState={setRgstNo}/>
 					</div>
 
 					<div className="flex_ box- flex_date">
 						<div>
-							<p className="table-caption body-2-B">발송 선택<span className="txt-violet-1">*</span></p>
-							<DropsMenu />
+							<p className="table-caption body-2-B">상태<span className="txt-violet-1">*</span></p>
+							<DropsMenu datas={send_datas} setState={setStatCd} default={statCd}/>
 						</div>
 						<div>
-							<p className="table-caption body-2-B">발송 선택<span className="txt-violet-1">*</span></p>
+							<p className="table-caption body-2-B">출원 일자<span className="txt-violet-1">*</span></p>
 							<DatePicker />
 						</div>
 					</div>
