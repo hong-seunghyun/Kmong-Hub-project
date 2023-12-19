852bf02620c6ba47abf29285a69f3f59b3abb63f
이재잰 <HINU0622@gmail.com>
2023-12-19 18:20:20 +0900

    보고서 업로드 구현

--- src/containers/technical_document/add_3.js
+++ src/containers/technical_document/add_3.js
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Badge from "/src/components/label/badge";
 import Input from "/src/components/textFields/textInput.tsx";
+import CompanyInput from "/src/components/searchBar/search_bar_company_management_menu"
 import ButtonL from "/src/components/buttons/button_outline_l"
 import Button from "/src/components/buttons/button_primary_l"
 import DropsMenu from "/src/components/dropsMenu/drops_menu";
@@ -12,7 +13,7 @@ import CheckBox from "/src/components/radio/checkbox"
 import { useEffect } from "react";
 import { useState } from "react";
 import { searchOrgn } from "../../asset/apis/signup";
-import { addPatent } from "../../asset/apis/tech";
+import { addPatent, addReport } from "../../asset/apis/tech";
 
 const Component = () => {
 
@@ -31,17 +32,14 @@ const Component = () => {
     }
   ];
 
-	const [ typeCd, setTypeCd ] = useState('R');
+	const [ typeCd, setTypeCd ] = useState('T');
   const [ tcqNm, setTcqNm ] = useState('');
   const [ rsacUcmdCd, setRsacUcmdCd ] = useState('');
-  const [ apyNo, setApyNo ] = useState('');
+	const [ uniqueNo, setUniqueNo ] = useState('');
   const [ apyAd, setApyAd ] = useState('');
-  const [ statCd, setStatCd ] = useState('');
-  const [ rgstNo, setRgstNo ] = useState('');
+  const [ sbmyn, setSbmyn ] = useState('');
   const [ ivtNm, setIvtNm ] = useState('');
-  const [ ipcVal, setIpcVal ] = useState('');
-  const [ cpcVal, setCpcVal ] = useState('');
-  const [ piuaYn, setPiuaYn ] = useState('');
+  const [ orgCntn, setOrgCntn ] = useState('');
   
   const [ data, setData ] = useState([]);
   const [ orgn, setOrgn ] = useState('');
@@ -55,8 +53,8 @@ const Component = () => {
 		});
 	}
 
-  const createThesis = () => {
-    addPatent({
+  const createReport = () => {
+    addReport({
       typeCd,
       tcqNm,
       rsacUcmdCd,
@@ -81,6 +79,7 @@ const Component = () => {
     } else if(typeCd === 'T') {
       window.location = '/technical_document/add_2';
     }
+		searchOrgan();
   }, [typeCd]);
 
 	return(
@@ -105,50 +104,50 @@ const Component = () => {
 							기본 정보
 					</div>
 					<div className="box-">
-						<Input labelText="기술명" placeholder="기술명을 입력해 주세요." helperTextResult="none" iconState="false"/>
+						<Input labelText="기술명" placeholder="기술명을 입력해 주세요." helperTextResult="none" iconState="false" state={tcqNm} setState={setTcqNm}/>
 					</div>
 
 					<div className="flex_ input-search box-">
-						<Input labelText="연구기관" placeholder="연구기관을 검색해 주세요." valueType="" helperTextResult="none" iconState="false"/>
+						<CompanyInput labelText="연구기관" placeholder="연구기관을 검색해 주세요." valueType="" helperTextResult="none" iconState="false" state={orgn} setState={setOrgn} data={data} setResult={setRsacUcmdCd}/>
 						<Icon icon="search" size={16} stroke="none" color="#574AFF" /> 
 					</div>
 
 					<div className="box-">
-						<Input importState="none" labelText="문서번호" placeholder="문서번호를 입력해 주세요." helperTextResult="none" iconState="false"/>
+						<Input importState="none" labelText="문서번호" placeholder="문서번호를 입력해 주세요." helperTextResult="none" iconState="false" state={uniqueNo} setState={setUniqueNo}/>
 					</div>
 
 					<div className="flex_ box- flex_date">
 						<div>
 							<p className="table-caption body-2-B">발행연도<span className="txt-violet-1">*</span></p>
-							<DatePicker />
+							<DatePicker setDate={setApyAd}/>
 						</div>
 					</div>
 
 					<div className="box-">
-						<Input labelText="발명자" placeholder="발명자를 입력해 주세요." helperTextResult="none" iconState="false"/>
+						<Input labelText="발명자" placeholder="발명자를 입력해 주세요." helperTextResult="none" iconState="false" state={ivtNm} setState={setIvtNm}/>
 					</div>
 
 					<div className="box-">
-						<Input labelText="출처" placeholder="출처를 입력해 주세요." helperTextResult="none" iconState="false"/>
+						<Input labelText="출처" placeholder="출처를 입력해 주세요." helperTextResult="none" iconState="false" state={orgCntn} setState={setOrgCntn}/>
 					</div>
 
 					<div className="box-">
 						<p className="table-caption body-2-B">첨부파일<span className="txt-violet-1">*</span></p>
-						<Upload state="default" type="normal" file=""/>
+						<Upload state="default" type="pdf" fileState={file} setFileState={setFile}/>
 						<p className="caption-R helper-txt">
 						파일 형식: <span>PDF, DOCX, HWP</span> <span className="bar">|</span> 최대 파일 크기: <span>500mb</span>
 						</p>
 					</div>
 					<div className="box- check-box">
-						<CheckBox size="small" label="개인정보 수집 및 이용에 동의합니다." />
+						<CheckBox size="small" label="개인정보 수집 및 이용에 동의합니다." setCheckState={setSbmyn}/>
 					</div>
 					<div className="btn-wrap flex_">
 						<div className="flex_">
 							<Link href="#">
 								<ButtonL text="초기화" />
 							</Link>
-							<Link href="/technical_document">
-								<Button text="저장" />
+							<Link href="/add_3">
+								<Button text="저장" onclick={createReport}/>
 							</Link>
 						</div>
 					</div>
