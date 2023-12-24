b148dc6ced862291ece1ed5e5b0cf016505a447e
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-24 17:53:41 +0900

    fix : build error

--- src/components/tabs/mypage_tab_fixed.js
+++ src/components/tabs/mypage_tab_fixed.js
@@ -1,8 +1,8 @@
-import React, {useRef, useState} from "react";
-import Link from 'next/link';
+import React, { useRef, useState } from "react";
+import Link from "next/link";
 
 const Component = (props) => {
-	const scrollRef = useRef(null);
+  const scrollRef = useRef(null);
 
   const [isDrag, setIsDrag] = useState(false);
   const [startX, setStartX] = useState();
@@ -18,70 +18,70 @@ const Component = (props) => {
   };
 
   const onDragMove = (e) => {
-		if (isDrag) {
-			const { scrollWidth, clientWidth, scrollLeft } = scrollRef.current;
-	
-			scrollRef.current.scrollLeft = startX - e.pageX;
-	
-			if (scrollLeft === 0) {
-				setStartX(e.pageX);
-			} else if (scrollWidth <= clientWidth + scrollLeft) {
-				setStartX(e.pageX + scrollLeft);
-			}
-		}
-	};
+    if (isDrag && scrollRef.current) {
+      const { scrollWidth, clientWidth, scrollLeft } = scrollRef.current;
 
-	const throttle = (func, ms) => {
-		let throttled = false;
-		return (...args) => {
-			if (!throttled) {
-				throttled = true;
-				setTimeout(() => {
-					func(...args);
-					throttled = false;
-				}, ms);
-			}
-		};
-	};
+      scrollRef.current.scrollLeft = startX - e.pageX;
 
-	const delay = 100;
-	const onThrottleDragMove = throttle(onDragMove, delay);
-	
-	return(
-		<div className="tab-container tab-scrollable" 
-			ref={scrollRef}  
-			onMouseDown={onDragStart}
-			onMouseMove={isDrag ? onThrottleDragMove : null}
-			onMouseUp={onDragEnd}
-			onMouseLeave={onDragEnd}
-		>
-			<ul className="flex_">
-				<li className={`tab-item body-2-B ${props.active === 0 ? "active" : ""}`}>
-					<span>
-						<Link href="/mypage">
-							내 정보 관리
-						</Link>
-					</span>
-				</li>
+      if (scrollLeft === 0) {
+        setStartX(e.pageX);
+      } else if (scrollWidth <= clientWidth + scrollLeft) {
+        setStartX(e.pageX + scrollLeft);
+      }
+    }
+  };
 
-				<li className={`tab-item body-2-B ${props.active === 1 ? "active" : ""}`}>
-					<span>
-						<Link href="/mypage/payment">
-						결제 플랜
-						</Link>
-					</span>
-				</li>
+  const throttle = (func, ms) => {
+    let throttled = false;
+    return (...args) => {
+      if (!throttled) {
+        throttled = true;
+        setTimeout(() => {
+          func(...args);
+          throttled = false;
+        }, ms);
+      }
+    };
+  };
 
-				<li className={`tab-item body-2-B ${props.active === 2 ? "active" : ""}`}>
-					<span>
-						<Link href="/mypage/history">
-						결제 내역
-						</Link>
-					</span>
-					
-				</li>
-			</ul>
-		</div>
-	)
-}
+  const delay = 100;
+  const onThrottleDragMove = throttle(onDragMove, delay);
+
+  return (
+    <div
+      className="tab-container tab-scrollable"
+      ref={scrollRef}
+      onMouseDown={onDragStart}
+      onMouseMove={isDrag ? onThrottleDragMove : null}
+      onMouseUp={onDragEnd}
+      onMouseLeave={onDragEnd}
+    >
+      <ul className="flex_">
+        <li
+          className={`tab-item body-2-B ${props.active === 0 ? "active" : ""}`}
+        >
+          <span>
+            <Link href="/mypage">내 정보 관리</Link>
+          </span>
+        </li>
+
+        <li
+          className={`tab-item body-2-B ${props.active === 1 ? "active" : ""}`}
+        >
+          <span>
+            <Link href="/mypage/payment">결제 플랜</Link>
+          </span>
+        </li>
+
+        <li
+          className={`tab-item body-2-B ${props.active === 2 ? "active" : ""}`}
+        >
+          <span>
+            <Link href="/mypage/history">결제 내역</Link>
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 export default Component;
