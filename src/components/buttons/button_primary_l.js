f85d376950ee5233b3318c209ba0cd6c7cc511b4
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-24 17:35:05 +0900

    fix : button components

--- src/components/buttons/button_primary_l.js
+++ src/components/buttons/button_primary_l.js
@@ -5,19 +5,19 @@ const Component = (props) => {
   const [isButton, setButton] = useState(false);
 
   const onClickActive = async () => {
-    if(props.state === 'disabled') return;
+    if (props.state === "disabled") return;
     setButton(!isButton);
-    props.onclick && await props.onclick();
+    props.onclick && (await props.onclick());
   };
   const ButtonPrimaryL = () => {
     return (
       <button
-        className={`btn_ radius-8 ac-violet btn-l body-2-R ${
-          isButton ? "pressed" : ""
-        } ${props.state}`}
+        className={`btn_ radius-8 ${
+          props.isActive ? "ac-violet" : "bg-gray"
+        } btn-l body-2-R ${isButton ? "pressed" : ""} ${props.state}`}
         onClick={onClickActive}
       >
-        <span className="flex_">
+        <span className={`flex_ ${!props.isActive && "txt-disabled"}`}>
           <Icon size={11.6} color="#fff" stroke="none" icon="plus" />
           {props.text}
           <Icon size={13} color="#fff" stroke="none" icon="chevronRight" />
