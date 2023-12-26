7872694e35ae0d455aa1d93b4188a9515e748e4e
bae0203 <gmbae06@dgsw.hs.kr>
2023-12-26 13:05:40 +0900

    test : 주석

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,36 +1,37 @@
 import React from "react";
 
-
 import ButtonPrimaryXL from "/src/components/buttons/button_primary_xl";
 import ButtonErrorXL from "/src/components/buttons/button_error_xl";
 import ButtonOutlineXL from "/src/components/buttons/button_outline_xl";
 
 const Component = (props) => {
-	return(
-		<div className="modal-container radius-20 flex_">
-			<img src="/images/file.png" alt="file" />
-			<div className="text-wrap flex_">
-				<h6 className="heading-3-B">{props.title}</h6>
-				<p className="body-3-R">{props.text}</p>
-			</div>
-			<div className={`btn-wrap ${props.type} ${props.value} ${props.count} flex_`}>
-				{/*
+  return (
+    <div className="modal-container radius-20 flex_">
+      <img src="/images/file.png" alt="file" />
+      <div className="text-wrap flex_">
+        <h6 className="heading-3-B">{props.title}</h6>
+        <p className="body-3-R">{props.text}</p>
+      </div>
+      <div
+        className={`btn-wrap ${props.type} ${props.value} ${props.count} flex_`}
+      >
+        {/*
 					props.count = 보여주는 버튼의 갯수를 적어줍니다 / one, two
 					props.value = 메인 버튼의 상태를 보여줍니다.(primary와 error) / default, error 
 					props.type = 버튼의 정렬방식을 나타냅니다. (한줄 정렬, 두줄 정렬) / full-button, half-button
 				*/}
-				<div className="primary-btn btn-box">
-				<ButtonPrimaryXL text="label"/>
-				</div>
-				<div className="error-btn btn-box">
-				<ButtonErrorXL text="label"/>
-				</div>
-				
-				<div className="outline-btn btn-box">
-				<ButtonOutlineXL text="label"/>
-				</div>
-			</div>
-		</div>
-	)
-}
-export default Component ;
+        <div className="primary-btn btn-box">
+          <ButtonPrimaryXL text="label" />
+        </div>
+        <div className="error-btn btn-box">
+          <ButtonErrorXL text="label" />
+        </div>
+
+        <div className="outline-btn btn-box">
+          <ButtonOutlineXL text="label" />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Component;
