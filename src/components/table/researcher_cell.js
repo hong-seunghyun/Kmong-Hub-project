37da4146561ac9ea4772f8c34c70754410178788
myeolinmalchi <rkd2274@pusan.ac.kr>
2023-12-26 15:11:06 +0900

    feat: jsdoc 추가하여 타입 체크 수행

--- src/components/table/researcher_cell.js
+++ src/components/table/researcher_cell.js
@@ -1,37 +1,63 @@
+// @ts-check
 import React from "react";
 import Link from "next/link";
-import Label from "/src/components/label/label"
+import ContentsStatusLabel from "/src/components/label/contents_save_status_label"
 import TextButtonUnderlinePrimaryS from "/src/components/buttons/text_button_underline_primary_s";
+import { CONTENTS_SAVE_STATUS } from "/src/asset/apis/contents/common/codes";
 
-const Component = (props) => {
-	return(
-		<div className="table-cell-container flex_ radius-8 body-3-R txt-third ">
-			<div className={`choice-cell ${props.choice? 'number':''}`}>
-				<input type="checkbox" />
-				<span>{props.number}</span>
-			</div>
-			<div className="category-cell">
-				<Label backgroundColor={props.labelBg} fontColor={props.labelColor} text={props.label} icon="false" iconColor="none"/>
-				<img src={props.img} />
-			</div>
-			<div className="title-cell">
-				{props.title}
-			</div>
-			<div className="belong-cell">
-				{props.belong}
-			</div>
-			<div className="writer-cell">
-				{props.writer}
-			</div>
-			<div className="position-cell">
-				{props.position}
-			</div>
-			<div className="etc-cell">
-				<Link style={{width:'auto'}} href={`${props.link}`}>
-					<TextButtonUnderlinePrimaryS text="조회"/>
-				</Link>
-			</div>
-		</div>
-	)
+/**
+ * @typedef ResearcherCellProps 
+ * @property {number} id
+ * @property {keyof typeof CONTENTS_SAVE_STATUS} saveStatus
+ * @property {string} img
+ * @property {string} name 
+ * @property {string} belong 소속
+ * @property {string} major 부서/학과
+ * @property {string} position 직책
+ */
+
+/**
+ * 연구자 리스트 테이블 셀
+ * @param {ResearcherCellProps} props
+ * @returns {JSX.Element}
+ */
+const Component = ({
+  id,
+  saveStatus,
+  img,
+  name,
+  belong,
+  major,
+  position
+}) => {
+  return (
+    <div className="table-cell-container flex_ radius-8 body-3-R txt-third ">
+      <div className={`choice-cell number`}>
+        <input type="checkbox" />
+        <span>{id}</span>
+      </div>
+      <div className="category-cell">
+        <ContentsStatusLabel saveStatus={saveStatus} />
+        <img src={img} />
+      </div>
+      <div className="title-cell">
+        {name}
+      </div>
+      <div className="belong-cell">
+        {belong}
+      </div>
+      <div className="writer-cell">
+        {major}
+      </div>
+      <div className="position-cell">
+        {position}
+      </div>
+      <div className="etc-cell">
+        <Link style={{ width: 'auto' }} href={`/researcher/detail?no=${id}`}>
+          <TextButtonUnderlinePrimaryS text="조회" />
+        </Link>
+      </div>
+    </div>
+  )
 }
 export default Component
--- src/components/table/technology_index_table_vertical_cell.js
+++ src/components/table/technology_index_table_vertical_cell.js
@@ -1,40 +1,71 @@
+// @ts-check
 import React from "react";
 import Link from "next/link";
-import Label from "/src/components/label/label"
+import ContentsStatusLabel from "../label/contents_save_status_label";
 import TextButtonUnderlinePrimaryS from "/src/components/buttons/text_button_underline_primary_s";
-import Icon from "/src/components/icon/icon.tsx";
+import Icon from "/src/components/icon/icon";
+import { CONTENTS_SAVE_STATUS } from "/src/asset/apis/contents/common/codes";
+import { convertDateToStr } from "src/util/dateUtil";
 
-const Component = (props) => {
-	return(
-		<div className="table-cell-container flex_ radius-8 body-3-R txt-third ">
-			<div className="pin-cell">
-				<div className={` ${props.pin}`}>
-					<Icon icon="pin" size={24} color="#B3B6B8" stroke="none" />
-				</div>
-			</div>
-			<div className={`choice-cell ${props.choice? 'number':''}`}>
-				<input type="checkbox" />
-				<span>{props.number}</span>
-			</div>
-			<div className="category-cell">
-				<Label backgroundColor={props.labelBg} fontColor={props.labelColor} text={props.label} icon="false" iconColor="none"/>
-				<img src={props.img} />
-			</div>
-			<div className="title-cell">
-			{props.title}
-			</div>
-			<div className="writer-cell">
-				{props.writer}
-			</div>
-			<div className="date-cell">
-				{props.date}
-			</div>
-			<div className="etc-cell">
-				<Link href={`${props.link}`} style={{width:'auto'}}>
-					<TextButtonUnderlinePrimaryS text="조회"/>
-				</Link>
-			</div>
-		</div>
-	)
+/**
+ * @typedef TechTableCellProps
+ * @type {Object}
+ * @property {`${boolean}`} pin
+ * @property {number} id
+ * @property {keyof typeof CONTENTS_SAVE_STATUS} [saveStatus]
+ * @property {string} [img]
+ * @property {string} title
+ * @property {string} writer
+ * @property {string} date
+ * @property {string} link
+ */
+
+/**
+ * 컨텐츠 검색 결과 테이블 행 컴포넌트
+ * @param {TechTableCellProps} props
+ * @returns {JSX.Element}
+ */
+const ContentsTableCell = ({
+  pin,
+  id,
+  saveStatus,
+  img,
+  title,
+  writer,
+  date, 
+  link
+}) => {
+  return (
+    <div className="table-cell-container flex_ radius-8 body-3-R txt-third ">
+      <div className="pin-cell">
+        <div className={`${pin}`}>
+          <Icon icon="pin" size={24} color="#B3B6B8" stroke="none" />
+        </div>
+      </div>
+      <div className={`choice-cell number`}>
+        <input type="checkbox" />
+        <span>{id}</span>
+      </div>
+      <div className="category-cell">
+        <ContentsStatusLabel saveStatus={saveStatus} />
+        <img src={img} />
+      </div>
+      <div className="title-cell">
+        {title}
+      </div>
+      <div className="writer-cell">
+        {writer}
+      </div>
+      <div className="date-cell">
+        {convertDateToStr(date)}
+      </div>
+      <div className="etc-cell">
+        <Link href={`${link}`} style={{ width: 'auto' }}>
+          <TextButtonUnderlinePrimaryS text="조회" />
+        </Link>
+      </div>
+    </div>
+  )
 }
-export default Component
+
+export default ContentsTableCell
